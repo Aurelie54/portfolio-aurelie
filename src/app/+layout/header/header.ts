585c794c5d69911740679c6e1065f80d38480cd8
Aurelie <aurelie.audemar@orange.fr>
2025-07-22 13:30:36 +0200

    Ajout primeng + header

---
+++ src/app/+layout/header/header.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
+import { ImageModule } from 'primeng/image';
+import { MenubarModule } from 'primeng/menubar';
+import { Toolbar } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButton } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, DividerModule, ImageModule, MenubarModule, Toolbar, ButtonModule, SplitButton, InputTextModule, IconField, InputIcon],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star'
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Components',
+            icon: 'pi pi-bolt'
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server'
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil'
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette'
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+      }
+    ]
+  }
+
+}
